f118bb04eb8fa31285827a56bb40c3d183841044
hellnomary <maria.porcires@outlook.com>
2023-09-19 11:31:54 +0200

    update

--- Dictionary/src/components/SearchWord.jsx
+++ Dictionary/src/components/SearchWord.jsx
@@ -7,16 +7,35 @@ function SearchWord() {
     async function handleClick() {
         const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
         const data = await response.json()
-        console.log(data[0].meanings);
+        /* console.log(data[0].meanings); */
 
-        setData(data)
-        console.log('ordet är', word);
+        if(Array.isArray(data) && data.length > 0){
+          setData(data)
+        } else {
+            setData([])
+        }
+        console.log('ordet är', data);
     }
 
     return(
         <section>
             <input type="text" id="searchWord" placeholder='search' onChange={(event) => setWord(event.target.value)}></input>
             <button onClick={handleClick}>Search word</button>
+
+            <div>
+             {data.map((wordData, index) => (
+                <div key={index}>
+                    <h3>{wordData.word}</h3>
+                    <p>{wordData.phonetic}</p>
+                    <h5>Meanings:</h5>
+                        <ul>
+                            {wordData.meanings.map((meaning, meaningIndex) => (
+                             <li key={meaningIndex}>{meaning.meanings}</li>
+                         ))}
+                        </ul>
+                 </div>
+                 ))}
+            </div>
         </section>
     )
 }
